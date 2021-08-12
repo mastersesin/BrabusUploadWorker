d232e38617695147b6bcd133f566c900c176ad9e
maste <mastersesin12345@gmail.com>
2021-08-12 23:20:01 +0700

    Final release

--- distribute_script.py
+++ distribute_script.py
@@ -29,6 +29,8 @@ virtualenv venv
 venv/bin/pip install -r requirements.txt
 sudo cp upload.service /etc/systemd/system/
 sudo systemctl start upload.service
+sudo cp checkplot.service /etc/systemd/system/
+sudo systemctl start checkplot.service
 """
 
 chia_plot_string = 'tmux new-session -d -s "myTempSession1" ./chia_plot -t /tmp1/tmp/ -2 /mnt/ram/ -d /tmp1/ -r 32 -n -1 -c xch1xsjaqskkkq6hvlvvall4042jeqd3jygarsrdnmrua7wjjtt0k5fqn9w8aa -f 95999787516a65e3afdd55a583001b56e7ec371f83ceb47f412779ef43460d9cd0d7583b9c3e99fd961c9591b4f95070'
