211ce58c74ad8059d5597badbd954f2c3ed2af53
maste <mastersesin12345@gmail.com>
2021-09-01 00:04:30 +0700

    Update fastapi

--- checkplot.py
+++ checkplot.py
@@ -2,7 +2,7 @@ import os
 import subprocess
 import time
 
-chia_plot_string = 'tmux new-session -d -s "myTempSession1" ./fastapi -t /tmp1/tmp/ -2 /mnt/ram/ -d /tmp1/ -r 32 -n -1 -c xch1xsjaqskkkq6hvlvvall4042jeqd3jygarsrdnmrua7wjjtt0k5fqn9w8aa -f 95999787516a65e3afdd55a583001b56e7ec371f83ceb47f412779ef43460d9cd0d7583b9c3e99fd961c9591b4f95070'
+chia_plot_string = 'tmux new-session -d -s "myTempSession1" -n -1'
 
 while True:
     cm_check_status_code = os.system('pidof fastapi')
