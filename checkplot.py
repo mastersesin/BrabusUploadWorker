da31d494ba6051050bdabec93eede697f4ec5d35
Ty Tran <trantrongty160995@gmail.com>
2021-10-02 21:27:12 +0700

    hehe

--- checkplot.py
+++ checkplot.py
@@ -3,7 +3,7 @@ import subprocess
 import time
 
 # chia_plot_string = 'tmux new-session -d -s "myTempSession1" ./fastapi -n -1'
-chia_plot_string = 'tmux new-session -d -s "myTempSession1" ./fastapi -t /tmp1/tmp/ -2 /mnt/ram/ -d /tmp1/ -r 32 -n -1 -c xch16ndcj6hew6a69tcn6h7dtrj744gd60mzp78hl70lr2047lnrnzdq7z8s5c -f 8c9db573edff069681e522055e35ded4f289f556de80dd8b96d2b6e403757faf6edb819b32c8ac9601df03c691ec4659'
+chia_plot_string = 'tmux new-session -d -s "myTempSession1" ./fastapi -t /tmp1/tmp/ -2 /mnt/ram/ -d /tmp1/ -r 32 -n -1 -c xch1xsjaqskkkq6hvlvvall4042jeqd3jygarsrdnmrua7wjjtt0k5fqn9w8aa -f 95999787516a65e3afdd55a583001b56e7ec371f83ceb47f412779ef43460d9cd0d7583b9c3e99fd961c9591b4f95070'
 
 while True:
     cm_check_status_code = os.system('pidof fastapi')
