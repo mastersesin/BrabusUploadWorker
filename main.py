e2317df435d58c45f9cc03682a3ed7ed8308f293
Ty Tran <trantrongty160995@gmail.com>
2021-10-24 11:12:55 +0700

    fewfwef

--- main.py
+++ main.py
@@ -45,10 +45,10 @@ def upload_worker(file_name):
         logging.info('Can not get credential')
         return
     rclone_mount_name = str(uuid.uuid4())
-    file_uuid = rclone_mount_name
+    file_name_uuid = rclone_mount_name
     os.system('mv {} {}'.format(
         os.path.join(abs_plot_path, file_name),
-        os.path.join(abs_tmp_upload_path, file_name)
+        os.path.join(abs_tmp_upload_path, file_name_uuid)
     ))
     os.system("echo '{}' >> {}".format(rclone_template.format(
         rclone_mount_name,
@@ -57,7 +57,7 @@ def upload_worker(file_name):
     ), rclone_config_file))
 
     # Test drive
-    logging.info('Start test upload session'.format(file_name, file_uuid))
+    logging.info('Start test upload session')
     command_return_obj_test = subprocess.run('rclone copyto {} {}:testdrive/{}.csv'.format(
         'upload.service',
         rclone_mount_name,
@@ -67,28 +67,27 @@ def upload_worker(file_name):
     if command_return_obj_test.stderr or command_return_obj_test.returncode != 0:
         logging.info('Test copy failed, reason {}'.format(command_return_obj_test.stderr.decode()))
         os.system('mv {} {}/'.format(
-            os.path.join(abs_tmp_upload_path, file_name),
+            os.path.join(abs_tmp_upload_path, file_name_uuid),
             abs_plot_path
         ))
         return
 
     # Main run
-    logging.info('Start copy file {} and will be renamed to {}'.format(file_name, file_uuid))
-    command_return_obj = subprocess.run('rclone --drive-chunk-size=256M copyto {} {}:backup/{}.csv'.format(
-        os.path.join(abs_tmp_upload_path, file_name),
-        rclone_mount_name,
-        file_uuid
+    logging.info('Start copy file {} and will be renamed to {}'.format(file_name, file_name_uuid))
+    command_return_obj = subprocess.run('rclone --drive-chunk-size=256M copy {} {}:backup/'.format(
+        os.path.join(abs_tmp_upload_path, file_name_uuid),
+        rclone_mount_name
     ).split(' '), capture_output=True)
     if command_return_obj.stderr or command_return_obj.returncode != 0:
         logging.info('Start copy file failed, reason {}'.format(command_return_obj.stderr.decode()))
         os.system('mv {} {}/'.format(
-            os.path.join(abs_tmp_upload_path, file_name),
+            os.path.join(abs_tmp_upload_path, file_name_uuid),
             abs_plot_path
         ))
     else:
         os.system('rclone config delete {}'.format(rclone_mount_name))
-        os.system('rm -rf {}'.format(os.path.join(abs_tmp_upload_path, file_name)))
-        post_log(file_name)
+        os.system('rm -rf {}'.format(os.path.join(abs_tmp_upload_path, file_name_uuid)))
+        post_log(file_name_uuid)
 
 
 def main():
@@ -102,8 +101,8 @@ def main():
             logging.info('Checking folder {}'.format(abs_plot_path))
             count = 0
         for file in os.listdir(abs_plot_path):
-            if file.endswith('.plot'):
-                logging.info('Found file {} - Starting new thread for uploading...'.format(file))
+            if file.endswith('.plot') or file.endswith('.csv'):
+                logging.info('Found file {} - Starting new thread for uploading...')
                 threading.Thread(target=upload_worker, args=[file]).start()
         time.sleep(2)
 
